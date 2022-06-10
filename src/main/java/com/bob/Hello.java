f427e0685dfa8851d11121eee70be7bdffaa2236
chou3934 <choumelanie12@gmail.com>
2022-06-10 13:56:39 +0800

    Hello and Person commit

---
+++ src/main/java/com/bob/Hello.java
@@ -0,0 +1,9 @@
+package com.bob;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Person person=new Person();
+        person.greeting();
+    }
+}
